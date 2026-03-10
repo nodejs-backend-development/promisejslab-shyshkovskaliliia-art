8a92acced85add62ed9cebd9e8a016f8e91a577b
shyshkovskaliliia <shyshkovska.liliia@chnu.edu.ua>
2026-03-10 22:27:38 +0200

    upgrade task2.7

---
+++ .github/02-makePromiseResolveWithValue/task2.7.js
@@ -0,0 +1,28 @@
+// ==================== ЗАВДАННЯ 2.7 ====================
+/**
+ * Створіть функцію-обгортку для синхронних функцій,
+ * яка перехоплює помилки та повертає проміс
+ * 
+ * @param {Function} fn - Синхронна функція
+ * @param {any[]} args - Аргументи для функції
+ * @returns {Promise<any>}
+ */
+function tryCatchPromise(fn, ...args) {
+    try{
+        const result = fn(...args);
+        return Promise.resolve(result);
+    } catch(error){
+        return Promise.reject(error);
+    }
+}
+
+// Перевірка:
+const goodFunction = (a, b) => a + b;
+const badFunction = () => { throw new Error('Oops!'); };
+
+tryCatchPromise(goodFunction, 5, 3)
+    .then(result => console.log(' Тест 2.7a:', result)); // 8
+
+tryCatchPromise(badFunction)
+    .catch(error => console.log(' Тест 2.7b:', error.message)); // 'Oops!'
+
